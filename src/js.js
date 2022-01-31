8494624058fa3c8e64d8a19b15fc61f86977c3e2
smsi <sinamehrabi80@gmail.com>
2022-01-31 06:25:54 +0330

    Add kardan moadel heab kardan

--- src/js.js
+++ src/js.js
@@ -9,13 +9,14 @@ document.querySelector("#save").addEventListener("click", function () {
     document.querySelector("#alert").style.color = "red";
   } else if (nomrehInput <= 20 && nomrehInput >= 0) {
     esmha.push(document.querySelector("#esm").value);
-    console.log(esmha);
     nomreha.push(Number(document.querySelector("#nomreh").value));
-    console.log(nomreha);
     document.querySelector("#nomrehdars").textContent =
       bishtarinnomreh(nomreha);
     document.querySelector("#esmedars").textContent =
       esmeBishtarinNomreh(esmha);
+    document.querySelector(
+      "#moadel"
+    ).textContent = `معدل برابر است با: ${moadelCac(nomreha)}`;
   } else {
     document.querySelector("#alert").textContent =
       "لطفا نمره خود را درست وارد کنید ";
@@ -36,4 +37,11 @@ function bishtarinnomreh(nomreha) {
 }
 function esmeBishtarinNomreh(darsha) {
   return darsha[indexNomreha];
+}
+function moadelCac(nomreha) {
+  moadel = 0;
+  for (let i = 0; i < nomreha.length; i++) {
+    moadel += nomreha[i];
+  }
+  return moadel / nomreha.length;
 }
